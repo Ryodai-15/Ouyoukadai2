4847ec7fecd1fd9a7b2c3745ecee358f1219776f
Ryodai-15 <gon.4629.1208@gmail.com>
2021-07-02 09:23:52 +0000

    [fix]kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,21 @@
 class BooksController < ApplicationController
+  
+  before_action :authenticate_user!
+  
+  before_action :correct_user, only: [:edit, :update]
+
 
   def show
     @book = Book.find(params[:id])
+    @new = Book.new
+    @user = User.find(current_user.id)
+
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -14,8 +24,9 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -34,7 +45,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destoy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -43,7 +54,15 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id]) # idをもとにPost（投稿）を特定
+    @user = @book.user             # 特定されたPostに紐づくUserを特定し、@userに入れる
+    if current_user != @user       # 現在ログインしているユーザー（編集者）と@user（投稿者）が異なったら
+      redirect_to books_path       # 一覧ページにリダイレクトさせる
+    end
   end
 
 end
